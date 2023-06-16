df08fe7d2bdb3bacc4d5184ca18ed05db6a4bb56
Tuvix Shih <tuvix@beehivedt.com>
2023-06-16 19:02:34 +0800

    feat: Add basic linebot function

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Data-soong
 
 Line-bot
+
+Reference:
+
+1. [line/line-bot-sdk-python: LINE Messaging API SDK for Python](https://github.com/line/line-bot-sdk-python)
--- app/main.py
+++ app/main.py
@@ -1,15 +1,50 @@
+import configparser
+from pathlib import Path
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+dir_path = Path(__file__).resolve().parent
+config = configparser.ConfigParser()
+config.read("{}/config.ini".format(dir_path))
+
+# Line Bot config
+CHANNEL_SECRET = config.get("Line", "channel_secret")
+CHANNEL_ACCESS_TOKEN = config.get("Line", "channel_access_token")
 
 app = FastAPI()
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+
+@app.post("/")
+async def echoBot(request: Request):
+    signature = request.headers["X-Line-Signature"]
+    body = await request.body()
+    try:
+        handler.handle(body.decode(), signature)
+    except InvalidSignatureError:
+        raise HTTPException(status_code=400, detail="Missing Parameters")
+    return "OK"
+
+
+@handler.add(MessageEvent, message=(TextMessage))
+def handling_message(event):
+    replyToken = event.reply_token
+
+    if isinstance(event.message, TextMessage):
+        messages = event.message.text
+
+        echoMessages = TextSendMessage(text=messages)
+        line_bot_api.reply_message(
+            reply_token=replyToken, messages=echoMessages
+        )
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"Hello": config.get("Line", "channel_access_token")}
